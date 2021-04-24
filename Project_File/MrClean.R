aaacd9c16a957680073f03d7168596928e16fc8d
Midiquin <Midiquin@users.noreply.github.com>
2021-04-24 17:29:43 -0400

    Update MrClean.R

--- Project_File/MrClean.R
+++ Project_File/MrClean.R
@@ -10,6 +10,9 @@ library(wordcloud)
 library(tidyr)
 library(lexicon)
 library(tidytext)
+library(ggplot2)
+
+v_list <- list2[-5]
 
 MrClean <- function(list){
   
@@ -26,30 +29,33 @@ MrClean <- function(list){
     
     list[[i]]$danceability <- replace_na(list[[i]]$danceability, list_avgs[[i]]$danceability)
     
-    #list[[i]]$key <- replace_na(list[[i]]$key, list_avgs[[i]]$key)
+    list[[i]]$key <- replace_na(list[[i]]$key, list_avgs[[i]]$key)
     
-    #list[[i]]$energy <- replace_na(list[[i]]$energy, list_avgs[[i]]$energy)
+    list[[i]]$energy <- replace_na(list[[i]]$energy, list_avgs[[i]]$energy)
     
-    #list[[i]]$loudness <- replace_na(list[[i]]$loudness, list_avgs[[i]]$loudness)
+    list[[i]]$loudness <- replace_na(list[[i]]$loudness, list_avgs[[i]]$loudness)
     
-    #list[[i]]$mode <- replace_na(list[[i]]$mode, list_avgs[[i]]$mode)
+    list[[i]]$mode <- replace_na(list[[i]]$mode, list_avgs[[i]]$mode)
     
-    #list[[i]]$speechiness <- replace_na(list[[i]]$speechiness, list_avgs[[i]]$speechiness)
+    list[[i]]$speechiness <- replace_na(list[[i]]$speechiness, list_avgs[[i]]$speechiness)
     
-    #list[[i]]$acousticness <- replace_na(list[[i]]$acousticness, list_avgs[[i]]$acousticness)
+    list[[i]]$acousticness <- replace_na(list[[i]]$acousticness, list_avgs[[i]]$acousticness)
     
-    #list[[i]]$instrumentalness <- replace_na(list[[i]]$instrumentalness, list_avgs[[i]]$instrumentalness)
+    list[[i]]$instrumentalness <- replace_na(list[[i]]$instrumentalness, list_avgs[[i]]$instrumentalness)
     
-    #list[[i]]$liveness  <- replace_na(list[[i]]$liveness , list_avgs[[i]]$liveness)
+    list[[i]]$liveness  <- replace_na(list[[i]]$liveness, list_avgs[[i]]$liveness)
+  
+    list[[i]]$valence <- replace_na(list[[i]]$valence, list_avgs[[i]]$valence)
     
-    #list[[i]]$valence <- replace_na(list[[i]]$valence, list_avgs[[i]]$valence)
-    
-    #list[[i]]$tempo <- replace_na(list[[i]]$tempo, list_avgs[[i]]$tempo)
+    list[[i]]$tempo <- replace_na(list[[i]]$tempo, list_avgs[[i]]$tempo)
   }
   
   return(list)
 }
 
-clean_list2 <- MrClean(list2)
+clean_list2 <- MrClean(v_list)
+
+
+
 
 
